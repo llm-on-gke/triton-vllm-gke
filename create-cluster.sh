24b34eb5d182a5d85ddd9cbb7bd7830b0e1c3641
Rick(Rugui) Chen <rickruguichen@google.com>
2024-02-21 11:42:57 -0500

    gemma added

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,32 +1,33 @@
-# for tesla-t4
-#gcloud container clusters create llama2-inference-cluster --num-nodes=1 --min-nodes=1 --max-nodes=3    --zone=us-west1-b     --accelerator="type=nvidia-tesla-t4,count=2,gpu-driver-version=latest"  --machine-type="n1-standard-8"  --enable-private-nodes --master-ipv4-cidr 172.16.0.32/28  --enable-ip-alias --scopes="gke-default,storage-rw"
-
-#tesla-l4:
-#gcloud container clusters create llama2-inference-cluster --num-nodes=1 --min-nodes=1 --max-nodes=3  --zone=us-central1-a     --accelerator="type=nvidia-l4,count=2,gpu-driver-version=default"  --machine-type="g2-standard-24" --enable-ip-alias --scopes="gke-default,storage-rw"
-
-# for L4 and spot private cluster
+# for L4 and spot node-pools
+export PROJECT_ID=<your-project-id>
 export REGION=us-central1
-export PROJECT_ID=$(gcloud config get project)
+export ZONE_1=${REGION}-a # You may want to change the zone letter based on the region you selected above
+export ZONE_2=${REGION}-b # You may want to change the zone letter based on the region you selected above
+export CLUSTER_NAME=triton-inference
+export NAMESPACE=triton
+gcloud config set project "$PROJECT_ID"
+gcloud config set compute/region "$REGION"
+gcloud config set compute/zone "$ZONE_1"
 
-gcloud container clusters create llm-inference-l4 --location ${REGION} \
+gcloud container clusters create $CLUSTER_NAME --location ${REGION} \
   --workload-pool ${PROJECT_ID}.svc.id.goog \
   --enable-image-streaming --enable-shielded-nodes \
   --shielded-secure-boot --shielded-integrity-monitoring \
   --enable-ip-alias \
-  --node-locations=$REGION-a \
+  --node-locations=$REGION-b \
   --workload-pool=${PROJECT_ID}.svc.id.goog \
   --addons GcsFuseCsiDriver   \
   --no-enable-master-authorized-networks \
   --machine-type n2d-standard-4 \
+  --cluster-version 1.28 \
   --num-nodes 1 --min-nodes 1 --max-nodes 3 \
   --ephemeral-storage-local-ssd=count=2 \
-  --enable-ip-alias \
-  --enable-private-nodes  \
-  --master-ipv4-cidr 172.16.0.32/28 \
   --scopes="gke-default,storage-rw"
 
-
-gcloud container node-pools create llm-inference-pool --cluster llm-inference-l4  --accelerator type=nvidia-l4,count=2,gpu-driver-version=latest   --machine-type g2-standard-24   --ephemeral-storage-local-ssd=count=2   --enable-autoscaling --enable-image-streaming   --num-nodes=0 --min-nodes=0 --max-nodes=3   --shielded-secure-boot   --shielded-integrity-monitoring   --node-locations $REGION-a,$REGION-b --region $REGION --spot
+gcloud container node-pools create $CLUSTER_NAME-pool --cluster \
+$CLUSTER_NAME --accelerator type=nvidia-l4,count=1,gpu-driver-version=latest   --machine-type g2-standard-8 \
+--ephemeral-storage-local-ssd=count=1   --enable-autoscaling --enable-image-streaming   --num-nodes=0 --min-nodes=0 --max-nodes=3 \
+--shielded-secure-boot   --shielded-integrity-monitoring --node-version=1.28 --node-locations $ZONE_1,$ZONE_2 --region $REGION --spot
 
 kubectl create ns triton
 kubectl create serviceaccount triton --namespace triton
